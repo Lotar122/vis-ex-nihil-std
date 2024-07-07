0d42727fd72cdefbb65d693fd867b69a4b12568e
Lotar122 <dajlizaka9@gmail.com>
2024-07-07 17:24:29 +0200

    i forgot to update...

--- headers/GenerateChecksum.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <fstream>
-
-namespace nihil::nstd {
-    uint32_t generateChecksum(const std::string file_path);
-}
--- nstd.hpp
+++ nstd.hpp
@@ -7,4 +7,6 @@
 #include "headers/OBJLoader.hpp"
 #include "headers/FS.hpp"
 #include "headers/ComponentSystem.hpp"
-#include "headers/GenerateChecksum.hpp"
+#include "headers/Checksum.hpp"
+#include "headers/utils.hpp"
+#include "headers/_voidp.hpp"
